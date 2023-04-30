04664070274a55d9002458638210a39c8a021ec8
JessFr12 <tx.franco6@gmail.com>
2023-04-30 01:14:48 -0500

    Only Q.11 for insert,update,delete

--- 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
+++ 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
@@ -1,2 +1,8 @@
 -- 11. Hollywood is remaking the classic movie "The Blob" and doesn't have a director yet. Add yourself to the person table, and assign yourself as the director of "The Blob" (the movie is already in the movie table). (1 record each)
+INSERT INTO person (person_name, birthday, biography)
+VALUES ('Jesse Franco', '01-01-2002', 'I love pizza');
 
+INSERT INTO movie (movie_id, director_id)
+VALUES ((SELECT movie_id FROM movie WHERE title = 'The Blob'), (SELECT person_id FROM person WHERE person_name = 'Jesse Franco'));
+
+SELECT * FROM person;
