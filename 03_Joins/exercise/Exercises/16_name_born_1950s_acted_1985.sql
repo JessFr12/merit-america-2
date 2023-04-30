60cd334974c634578a1b488691c560fe8969b834
JessFr12 <tx.franco6@gmail.com>
2023-04-29 19:31:23 -0500

    ALmost finish JOINS assignment

--- 03_Joins/exercise/Exercises/16_name_born_1950s_acted_1985.sql
+++ 03_Joins/exercise/Exercises/16_name_born_1950s_acted_1985.sql
@@ -1,4 +1,7 @@
 -- 16. The names and birthdays of actors born in the 1950s who acted in movies that were released in 1985.
 -- Order the results by actor from oldest to youngest.
 -- (20 rows)
-
+SELECT person_name, birthday
+FROM person
+JOIN movie ON person.person_id = movie.director_id
+WHERE birthday BETWEEN '19500101' AND '19591231';
