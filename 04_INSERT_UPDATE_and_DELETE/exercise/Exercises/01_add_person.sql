963bbee05798093cd24226f3ce1221c71eb6c4ca
JessFr12 <tx.franco6@gmail.com>
2023-04-30 00:09:39 -0500

    01-05 Pass on insert,update,delete

--- 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/01_add_person.sql
+++ 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/01_add_person.sql
@@ -1,2 +1,10 @@
 -- 1. Add a new person to the person table with the name "Lisa Byway" with the birthday of 4/15/1984 (1 row)
 
+SELECT * FROM person;
+
+INSERT INTO person (person_name, birthday)
+VALUES ('Lisa Byway', '04-15-1984');
+
+SELECT * FROM person
+WHERE person_name = 'Lisa Byway';
+
--- 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/03_add_actor.sql
+++ 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/03_add_actor.sql
@@ -1,2 +1,10 @@
 -- 3. Did you know Eric Stoltz was originally cast as Marty McFly in "Back to the Future"? Add Eric Stoltz to the list of actors for "Back to the Future" (1 row)
 
+SELECT person_name FROM person
+JOIN movie_actor ON person.person_id = movie_actor.actor_id
+JOIN movie ON movie_actor.movie_id = movie.movie_id
+WHERE movie.title = 'Back to the Future';
+
+INSERT INTO movie_actor (movie_id, actor_id)
+VALUES ((SELECT movie_id FROM movie WHERE title = 'Back to the Future'), (SELECT person_id FROM person
+																		 WHERE person_name = 'Eric Stoltz'));
