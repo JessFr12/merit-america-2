c89ba604127316d5148e1c03f465b40682cc9297
JessFr12 <tx.franco6@gmail.com>
2023-05-07 03:28:27 -0500

    Finish methods for JDBCProjectDAO

--- 07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcProjectDao.java
+++ 07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcProjectDao.java
@@ -2,6 +2,7 @@ package com.techelevator.projects.dao;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import javax.sql.DataSource;
 
@@ -20,23 +21,61 @@ public class JdbcProjectDao implements ProjectDao {
 
 	@Override
 	public Project getProject(int projectId) {
-		return new Project(0, "Not Implemented Yet", null, null);
+		Project project = null;
+		String sql = "SELECT project_id, name, from_date, to_date FROM project " +
+				"WHERE project_id = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql,projectId);
+		if (results.next()) {
+			project = mapRowToProject(results);
+			return project;
+		} else
+			return null;
+
 	}
 
 	@Override
 	public List<Project> getAllProjects() {
-		return new ArrayList<>();
+		List<Project> projects = new ArrayList<>();
+		String sql = "SELECT project_id, name, from_date, to_date FROM project;";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		while(results.next()) {
+			Project project = mapRowToProject(results);
+			projects.add(project);
+		}
+		return projects;
 	}
 
 	@Override
 	public Project createProject(Project newProject) {
-		return null;
+		String sql = "INSERT INTO project (name, from_date, to_date) " +
+				"VALUES (?, ?, ?) RETURNING project_id;";
+		Long newProjectId = jdbcTemplate.queryForObject(sql, Long.class, newProject.getName(),
+				newProject.getFromDate(), newProject.getToDate());
+		return getProject(newProjectId.intValue());
 	}
 
 	@Override
 	public void deleteProject(int projectId) {
+		String deleteFromTimesheet = "DELETE FROM timesheet WHERE project_id = ?;";
+		jdbcTemplate.update(deleteFromTimesheet,projectId);
 
+		String deleteFromProjectEmp = "DELETE FROM project_employee WHERE project_id = ?;";
+		jdbcTemplate.update(deleteFromProjectEmp,projectId);
+
+		String sql = "DELETE FROM project " +
+				"WHERE project_id = ?;";
+		jdbcTemplate.update(sql,projectId);
 	}
-	
+
+	private Project mapRowToProject(SqlRowSet rowSet) {
+		Project project = new Project();
+		project.setId(rowSet.getInt("project_id"));
+		project.setName(rowSet.getString("name"));
+		project.setFromDate(rowSet.getDate("from_date") == null ? null :
+				rowSet.getDate("from_date").toLocalDate());
+		project.setToDate(rowSet.getDate("to_date") == null ? null :
+				rowSet.getDate("to_date").toLocalDate());
+		return project;
+	}
 
 }
