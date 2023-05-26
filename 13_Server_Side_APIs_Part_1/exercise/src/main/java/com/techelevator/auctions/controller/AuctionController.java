9ae71a99ac258c8caef51aba3dea3ff43d1deee0
Jesse Franco <108185010+JessFr12@users.noreply.github.com>
2023-05-26 18:22:31 -0500

    Implement list() method

--- 13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ 13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,16 +2,25 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
 public class AuctionController {
 
     private AuctionDao dao;
+    private List<Auction> auctions = new ArrayList<>();
 
     public AuctionController() {
         this.dao = new MemoryAuctionDao();
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Auction> list() {
+        return auctions;
     }
 
 }
